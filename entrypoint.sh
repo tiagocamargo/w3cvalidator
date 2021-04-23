6a6022fcb86fa631ffb66f352c3bdb93f7a889ad
Marcus Lindblom <m@marcuslindblom.com>
2021-04-23 07:30:19 +0200

    Tidy up shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ LEVEL=${2:-"error"}
 HOST=${3:-"https://validator.w3.org/nu/"}
 
 
-RESULT=`curl -L -X GET ''"$HOST"'?doc='"$URL"'/&out=json&level='"$LEVEL"'' | jq -r '.messages' | jq length`
+RESULT=$(curl -L -X GET ''"$HOST"'?doc='"$URL"'/&out=json&level='"$LEVEL"'' | jq -r '.messages' | jq length)
 
 echo "::set-output name=count::$RESULT"
 
-if [ $RESULT -gt 0 ]; then
+if [ "$RESULT" -gt 0 ]; then
 	exit 1
 else
 	exit 0

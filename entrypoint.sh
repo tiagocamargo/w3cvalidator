027697bd096e98691a502031fa10b6cfff670249
Marcus Lindblom <m@marcuslindblom.com>
2021-04-23 13:04:26 +0200

    Adds fix for validating css

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ LEVEL=${2:-"error"}
 HOST=${3:-"https://validator.w3.org/nu/"}
 
 
-RESULT=$(curl -L -X GET ''"$HOST"'?doc='"$URL"'/&out=json&level='"$LEVEL"'' | jq -r '.messages' | jq length)
+RESULT=$(curl -L -X GET ''"$HOST"'?doc='"$URL"'&out=json&level='"$LEVEL"'' | jq -r '.messages' | jq length)
 
 echo "::set-output name=count::$RESULT"
 

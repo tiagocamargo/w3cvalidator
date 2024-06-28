4c0e21c8065242ef90084b6539464f09163805fe
Tiago Camargo <tiago.camargo@portabilis.com.br>
2024-06-28 14:37:43 -0300

    Update entrypoint.sh
    Ajusta o script para mostrar os erros.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,7 @@
-#!/bin/bash
-
 URL=$1
 LEVEL=${2:-"error"}
 HOST=${3:-"https://validator.w3.org/nu/"}
 
+RESULT=$(curl -sS -L -X GET ''"$HOST"'?doc='"$URL"'&out=json&level='"$LEVEL"'' | jq -r )
 
-RESULT=$(curl -L -X GET ''"$HOST"'?doc='"$URL"'&out=json&level='"$LEVEL"'' | jq -r '.messages' | jq length)
-
-echo "::set-output name=count::$RESULT"
-
-if [ "$RESULT" -gt 0 ]; then
-	exit 1
-else
-	exit 0
-fi
+echo $RESULT

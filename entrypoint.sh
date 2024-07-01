bbfcd33d362abf37b1f9b7416e8248d0ce84cd70
Tiago Camargo <tiago.camargo@portabilis.com.br>
2024-07-01 14:47:51 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-URL=$1
-LEVEL=${2:-"error"}
-HOST=${3:-"https://validator.w3.org/nu/"}
+#URL=$1
+#LEVEL=${2:-"error"}
+#HOST=${3:-"https://validator.w3.org/nu/"}
 
-RESULT=$(curl -sS -L -X GET ''"$HOST"'?doc='"$URL"'&out=json&level='"$LEVEL"'' | jq -r )
+#RESULT=$(curl -sS -L -X GET ''"$HOST"'?doc='"$URL"'&out=json&level='"$LEVEL"'' | jq -r )
 
-echo $RESULT
+#echo $RESULT
+
+
+# Inicializa a variável de porcentagem
+percent=0
+
+# Loop que incrementa a porcentagem a cada segundo
+while [ $percent -le 100 ]; do
+  # Limpa a linha anterior
+  echo -ne "\r"
+
+  # Exibe a porcentagem atual
+  echo -ne "Validando: $percent%"
+
+  # Incrementa a porcentagem
+  ((percent++))
+
+  # Aguarda um segundo
+  sleep 1
+done
+
+# Exibe uma mensagem de conclusão
+echo -e "\nValidação completa!"
+
+
